cd5bba97eec45f0c094c1472a94ec8d78d09fa1d
ವಿನಯ್ ಕಿರಣ್ ಪಾಟೀಲ ( Vinay Kiran P) <vinaykiranp888@gmail.com>
2023-08-11 17:35:50 +0530

    Updated all fetchAPI's

--- src/main/java/com/vk/quiz/controller/QuestionController.java
+++ src/main/java/com/vk/quiz/controller/QuestionController.java
@@ -1,9 +1,11 @@
 package com.vk.quiz.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,15 @@ public class QuestionController {
     @GetMapping("/allQuestions")
     public List<Question> getAllQuestions(){
         return questionService.getAllQuestions();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Question> getQuestionById(@PathVariable Integer id){
+        return questionService.getQuestionsById(id);
+    }
+
+    @GetMapping("/category/{category}")
+    public List<Question> getQuestionByCategory(@PathVariable("category") String category){
+        return questionService.getQuestionsByCategory(category);
     }
 }
---
+++ src/main/java/com/vk/quiz/service/QuestionService.java
@@ -0,0 +1,28 @@
+package com.vk.quiz.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vk.quiz.dao.QuestionDao;
+import com.vk.quiz.model.Question;
+
+@Service
+public class QuestionService {
+    @Autowired
+    private QuestionDao questionDao;
+
+    public List<Question> getAllQuestions(){
+        return questionDao.findAll();
+    }
+
+    public Optional<Question> getQuestionsById(Integer id) {
+        return questionDao.findById(id);
+    }
+    
+    public List<Question> getQuestionsByCategory(String category) {
+        return questionDao.findByCategory(category);
+    }
+}
