ba6fb49017d436bfb06a820bdaf07331d41a36fe
ವಿನಯ್ ಕಿರಣ್ ಪಾಟೀಲ ( Vinay Kiran P) <vinaykiranp888@gmail.com>
2023-08-11 14:16:02 +0530

    Updated url question/allQuestions to print string

---
+++ src/main/java/com/vk/quiz/controller/QuestionController.java
@@ -0,0 +1,15 @@
+package com.vk.quiz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @GetMapping("/allQuestions")
+    public String getAllQuestions(){
+        return "Vk";
+    }
+}
